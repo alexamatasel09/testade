bfbe80ac3a1f8972d60da2e95342c87b4e703c3a
alexamatasel09 <cristeamariaalexandra@gmail.com>
2025-08-04 11:13:18 +0300

    Update main.tf

--- ade/main.tf
+++ ade/main.tf
@@ -1,1 +1,18 @@
-test
+provider "azurerm" {
+  features {}
+}
+
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
+variable "resourceGroupName" {
+  type    = string
+  default = "myResourceGroup"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resourceGroupName
+  location = var.location
+}
